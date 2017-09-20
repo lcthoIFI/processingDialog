fe824459610f068eb7252284ec09063ac0895b0c
luongchitho <luongchitho08t2@gmail.com>
2017-09-20 17:54:09 +0700

    "Process combine sentence this "

--- proces_dialog.py
+++ proces_dialog.py
@@ -4,11 +4,18 @@
 import time
 from dateutil.parser import parse
 import os
+import re
+
+def removeSpecificCharacters(text):
+    return re.sub('[!@#$\n<>]', '', text)
 
 def isTimeFormat(input):
     try:
-        parse(input)
-        return True
+        try:
+            parse(input)
+            return True
+        except Exception as e:
+            print()
     except ValueError:
         return False
 
@@ -20,8 +27,10 @@ def checkStringTime(listText):
     return listTemp
 
 def getIDtextList(mylist):
+
     indices = [i for i, s in enumerate(mylist) if '--- Conversation ---' in s]
-    return indices[0]
+    if indices != [] :return indices[0]
+    else: return None
 
 ### TODO
 def correctSentenceQA(Ldialog):
@@ -29,38 +38,45 @@ def correctSentenceQA(Ldialog):
     sentence = ''
     for item in Ldialog:
         for id in range(len(item)):
-            splittext = item[id].split('\t')
+            splittext = removeSpecificCharacters(item[id]).split('\t')
+            if len(splittext) > 1 and splittext[len(splittext) -1] =='\n':
+                continue
             ##
-            #simulair = 0
             splitTextNext = []
             if id < len(item) - 1:
-                splitTextNext = item[id + 1].split('\t')
-                #if splittext[1]
+                splitTextNext = removeSpecificCharacters(item[id + 1]).split('\t')
+            splitTextPre = []
             if id > 0:
-                splitTextPre = item[id -1].split('\t')
-
+                splitTextPre = removeSpecificCharacters(item[id -1]).split('\t')
             if isTimeFormat(splittext[0]):
                 if id == len(item) - 1:
-                    sentence = item[id]
+                    sentence = item[id].strip('\n')
                     sentenceSplit = sentence.split('\t')
                     listdialog.append(checkStringTime(sentenceSplit))
                     sentence = ''
-                elif splitTextNext != [] and isTimeFormat(splitTextNext[0]):
-                    if splitTextNext[1].strip() == splittext[1].strip():
-                        ## TODO
-                        if len(splitTextPre) > 1 and splittext[1] == splitTextPre[1]:
-                            sentence = sentence + ' ' + splittext[2]
-                        else: sentence = item[id]
-                    else:
-                        sentence = item[id]
-                        sentenceSplit = sentence.split('\t')
-                        listdialog.append(checkStringTime(sentenceSplit))
-                        sentence = ''
+                elif (splitTextNext != [] or len(splitTextNext) < 2) and isTimeFormat(splitTextNext[0]):
+                    try:
+                        if splitTextNext[1].strip() == splittext[1].strip():
+                            ## TODO
+                            if len(splitTextPre) > 1 and splittext[1] == splitTextPre[1]:
+                                sentence = sentence + ' ' + splittext[2].strip('\n')
+                            else: sentence = item[id].strip('\n')
+                        else:
+                            sentence = item[id].strip('\n')
+                            sentenceSplit = sentence.split('\t')
+                            listdialog.append(checkStringTime(sentenceSplit))
+                            sentence = ''
+                    except Exception as e:
+                        print(splitTextNext)
+                        print(splittext)
                 else:
-                    sentence = item[id]
+                    ##TODO
+                    if len(splitTextPre) > 1 and len(splittext) > 2 and len(splitTextPre) > 2 and splittext[1] == splitTextPre[1]:
+                        sentence = sentence + ' ' + splittext[2].strip('\n')
+                    else:
+                        sentence = item[id].strip('\n')
             else:
-                sentence = sentence + ' ' + item[id]
-                #if id < len(item) - 1:
+                sentence = sentence + ' ' + item[id].strip('\n')
                 if splitTextNext != [] and isTimeFormat(splitTextNext[0]):
                     splitSentence = sentence.split('\t')
                     listdialog.append(checkStringTime(splitSentence))
@@ -76,6 +92,8 @@ def read_data(fileName):
     with open(fileName,"r") as file:
         content = file.readlines()
         seuil = getIDtextList(content)
+        if seuil == None:
+            return listDialog
         for id in range(len(content)):
             if id > seuil and content[id] != '\n':
                 listDialog.append(content[id])
@@ -90,16 +108,28 @@ def getListData():
     listData = []
     listNameFile = os.listdir('data')
     for path in listNameFile:
-        listData.append(read_data('data/'+ path))
+        if read_data('data/'+ path) != None:
+            listData.append(read_data('data/'+ path))
+        else:
+            continue
     return listData
 
 def extract_respons_sentenses(listDt):
     dictionary = {}
     for item in listDt:
-        for id in range(len(item)):
-            t = item[id][0]
-            if item[id][0] == 'FPT Telecom':
-                dictionary[item[id-1][1]] = item[id][1]
+        if len(item) < 2 :
+            continue
+        else:
+            for id in range(len(item)):
+                if len(item[id]) < 2:
+                    continue
+                t = item[id][0]
+                if item[id][0] == 'FPT Telecom':
+                    if len(item[id]) > 1 and id > 0:
+                        try:
+                            dictionary[item[id-1][1]] = item[id][1]
+                        except Exception as e :
+                            continue
     return dictionary
 
 # if __name__ == "__main__":
